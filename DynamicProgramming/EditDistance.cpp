2d2cc25ed8f31a45a6621153771e66645fa7ccbd
AyushBhandariNITK <ayushbhandari8909@gmail.com>
2020-08-08 18:46:46 +0530

    Google,LinkedIn,Microsoft,Amazon

---
+++ DynamicProgramming/EditDistance.cpp
@@ -0,0 +1,21 @@
+//https://www.interviewbit.com/problems/edit-distance/
+int Solution::minDistance(string A, string B)
+{
+    int n=A.length(),m=B.length();
+    vector<vector<int>> dp(n+1,vector(m+1,0));
+    for(int i=0;i<=n;i++)
+        dp[i][0]=i;
+    for(int j=0;j<=m;j++)
+        dp[0][j]=j;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=m;j++)
+        {   
+            if(A[i-1]==B[j-1])
+                dp[i][j]=dp[i-1][j-1];
+            else
+                dp[i][j]=1+min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]));
+        }
+    }
+    return dp[n][m];
+}
